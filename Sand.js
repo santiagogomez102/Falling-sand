170d143df18861ae50e7b65edf3e7b9cea15f8b3
santiagogomez102 <santiago.gomez102@outlook.com>
2024-04-06 13:39:32 -0500

    sand can't go bellow grid

--- Sand.js
+++ Sand.js
@@ -48,9 +48,11 @@ function draw() {
             let state = grid[i][j];
             if (state === 1) {
                 let bellow = grid[i][j + 1];
-                if (bellow === 0) {
+                if (bellow === 0 && j < rows - 1) {
                     nextGrid[i][j] = 0;
                     nextGrid[i][j + 1] = 1;
+                } else {
+                    nextGrid[i][j] = 1;
                 }
             }
         }
