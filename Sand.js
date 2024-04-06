5b69ab67d85311f4715481b204e1653270f45982
santiagogomez102 <santiago.gomez102@outlook.com>
2024-04-06 13:21:25 -0500

    Canvas and grid created

--- Sand.js
+++ Sand.js
@@ -1,17 +1,38 @@
-"use strict"
-
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
-    for (let i = 0; i < arr.lenght; i++) {
+    for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows);
     }
     return arr;
 }
 
+let grid;
+let w = 10;
+let cols, rows;
+
 function setup() {
     createCanvas(400, 400);
+    cols = width / w;
+    rows = height / w;
+    grid = make2DArray(cols, rows);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = 0;
+        }
+    }  
 }
 
 function draw() {
-    background(220);
+    background(0);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            stroke(255);
+            fill(grid[i][j]*255);
+            let x = i * w;
+            let y = j * w;
+            square(x, y, w);
+        }
+    }
 }
