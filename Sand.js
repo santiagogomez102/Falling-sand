0c0a95cdde1adbacfed090f56fcad850989ab456
santiagogomez102 <santiago.gomez102@outlook.com>
2024-04-11 20:56:07 -0500

    A matrix falls, not a grain

--- Sand.js
+++ Sand.js
@@ -33,10 +33,21 @@ function mousePressed(){
 }
 
 function mouseDragged(){
-    let col = floor(mouseX / w);
-    let row = floor(mouseY / w);
-    if (col >= 0 && col <= cols-1 && row >= 0 && row <= rows-1){
-        grid[col][row] = 1;
+    let mouseCol = floor(mouseX / w);
+    let mouseRow = floor(mouseY / w);
+  
+    let matrix = 5;
+    let extent = floor(matrix / 2);
+    for (let i = -extent; i <= extent; i++) {
+        for (let j = -extent; j <= extent; j++) {
+            if (random(1) < 0.75) {
+                let col = mouseCol + 1;
+                let row = mouseRow + j;
+                if (col >= 0 && col <= cols-1 && row >= 0 && row <= rows-1){
+                    grid[col][row] = 1;
+                }
+            }
+        }
     }
 }
 
