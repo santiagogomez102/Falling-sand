01843b3e897f30e061efc8aa5f90f3dcc6cd0e98
santiagogomez102 <santiago.gomez102@outlook.com>
2024-04-11 21:13:26 -0500

    Final code

--- Sand.js
+++ Sand.js
@@ -1,112 +1,131 @@
+// Create a 2D array
+// Sorry if you are used to matrix math!
+// How would you do this with a
+// higher order function????
+
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows);
-      for (let j = 0; j < arr[i].length; j++){
+      arr[i] = new Array(rows);
+      // Fill the array with 0s
+      for (let j = 0; j < arr[i].length; j++) {
         arr[i][j] = 0;
       }
     }
     return arr;
-}
-
-let grid;
-let w = 10;
-let cols, rows;
-
-let hueValue = 200;
-
-function setup() {
-    createCanvas(900, 700);
+  }
+  
+  // The grid
+  let grid;
+  // How big is each square?
+  let w = 5;
+  let cols, rows;
+  let hueValue = 200;
+  
+  // Check if a row is within the bounds
+  function withinCols(i) {
+    return i >= 0 && i <= cols - 1;
+  }
+  
+  // Check if a column is within the bounds
+  function withinRows(j) {
+    return j >= 0 && j <= rows - 1;
+  }
+  
+  function setup() {
+    createCanvas(500, 300);
     colorMode(HSB, 360, 255, 255);
     cols = width / w;
     rows = height / w;
     grid = make2DArray(cols, rows);
-
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            grid[i][j] = 0;
-        }
-    }
-}
-
-function mousePressed(){
-    let col = floor(mouseX / w);
-    let row = floor(mouseY / w);
-    grid[col][row] = 1;
-}
-
-function mouseDragged(){
+  }
+  
+  function mouseDragged() {
     let mouseCol = floor(mouseX / w);
     let mouseRow = floor(mouseY / w);
-  
+    
+    // Randomly add an area of sand particles
     let matrix = 5;
     let extent = floor(matrix / 2);
     for (let i = -extent; i <= extent; i++) {
-        for (let j = -extent; j <= extent; j++) {
-            if (random(1) < 0.75) {
-                let col = mouseCol + 1;
-                let row = mouseRow + j;
-                if (col >= 0 && col <= cols-1 && row >= 0 && row <= rows-1){
-                    grid[col][row] = hueValue;
-                }
-            }
+      for (let j = -extent; j <= extent; j++) {
+        if (random(1) < 0.75) {
+          let col = mouseCol + i;
+          let row = mouseRow + j;
+          if (withinCols(col) && withinRows(row)) {
+            grid[col][row] = hueValue;
+          }
         }
+      }
     }
+    // Change the color of the sand over time
     hueValue += 1;
     if (hueValue > 360) {
-        hueValue = 0;
+      hueValue = 1;
     }
-}
-
-function draw() {
-    background(0);
-
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            noStroke();
-            if (grid[i][j] > 0) {
-                fill(grid[i][j], 255, 255);
-                let x = i * w;
-                let y = j * w;
-                square(x, y, w);
-            }
-        }
-    }
+  }
   
-    let nextGrid = make2DArray(cols, rows);
+  function draw() {
+    background(0);
+    
+    // Draw the sand
     for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            let state = grid[i][j];
-            if (state > 0) {
-                let below = grid[i][j + 1];
-              
-                let dir = 1;
-                if (random(1) < 0.5) {
-                    dir *= -1;
-                }
-                
-                let belowA, belowB;
-              
-                if (i + dir >= 0 && i + dir <= cols - 1){
-                    belowA = grid[i + dir][j + 1];
-                }
-              
-                if (i - dir >= 0 && i - dir <= cols - 1){
-                    belowB = grid[i - dir][j + 1];
-                }
-              
-                if (below === 0) {
-                    nextGrid[i][j + 1] = grid[i][j];
-                } else if (belowA === 0) {
-                    nextGrid[i + dir][j+1] = grid[i][j];
-                } else if (belowB === 0) {
-                    nextGrid[i - dir][j+1] = grid[i][j];
-                }
-                else {
-                    nextGrid[i][j] = grid[i][j];
-                }
-            }
+      for (let j = 0; j < rows; j++) {
+        noStroke();
+        if (grid[i][j] > 0) {
+          fill(grid[i][j], 255, 255);
+          let x = i * w;
+          let y = j * w;
+          square(x, y, w);
         }
+      }
     }
-    grid = nextGrid; 
-}
+    
+    // Create a 2D array for the next frame of animation
+    let nextGrid = make2DArray(cols, rows);
+    
+    // Check every cell
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows ; j++) {
+        // What is the state?
+        let state = grid[i][j];
+        
+        // If it's a piece of sand!
+        if (state > 0) {
+          // What is below?
+          let below = grid[i][j + 1];
+          
+          // Randomly fall left or right
+          let dir = 1;
+          if (random(1) < 0.5) {
+            dir *= -1;
+          }
+          
+          // Check below left or right
+          let belowA = -1;
+          let belowB = -1;
+          if (withinCols(i + dir)) {
+            belowA = grid[i + dir][j + 1];
+          }
+          if (withinCols(i - dir)) {
+            belowB = grid[i - dir][j + 1];
+          }
+          
+          
+          // Can it fall below or left or right?
+          if (below === 0) {
+            nextGrid[i][j + 1] = state;
+          } else if (belowA === 0) {
+            nextGrid[i + dir][j + 1] = state;
+          } else if (belowB === 0) {
+            nextGrid[i - dir][j + 1] = state;
+          // Stay put!
+          } else {
+            nextGrid[i][j] = state;
+          }
+        }
+      }
+    }
+    grid = nextGrid;
+  }
+  
